9f5f1b7991a3b1d168b5b5eabcb707d44030a42b
Ionut <ipetre986@gmail.com>
2025-11-09 23:24:30 +0200

    Update la toate

---
+++ a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define NMAX 1005
+#define ll long long
+
+std::ifstream f("singur.in");
+std::ofstream g("singur.out");
+
+using namespace std;
+
+int main() {
+    cout << "Hello world!" << endl;
+    return 0;
+}
